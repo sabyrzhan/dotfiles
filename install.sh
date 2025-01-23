72ad7c6491ff74b94b1fd94057ffc96b4225859f
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2025-01-23 20:58:49 +0200

    update install.sh, disable multipass install

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
+#!/usr/bin/env zsh
+
 set -e
+echo "Installing brews..."
 brew bundle install --file=$PWD/Brewfile
+echo ""
+
+echo "Installing fnm..."
 fnm install --lts && fnm use lts-latest
+echo ""
 
 # omz install, powerlevel10k theme install
+echo "Installing zsh configs..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo ""
 
 # supdock
+echo "Installing supdock..."
 npm install -g supdock
+echo ""
 
-#shothand kubectl
+#shothand fubectl
+echo "Installing fubectl..."
 curl -L https://rawgit.com/kubermatic/fubectl/master/fubectl.source -o $HOME/fubectl.source
+echo "Installing krew..."
 #krew
 (
   set -x; cd "$(mktemp -d)" &&
@@ -21,6 +34,6 @@ curl -L https://rawgit.com/kubermatic/fubectl/master/fubectl.source -o $HOME/fub
   tar zxvf "${KREW}.tar.gz" &&
   ./"${KREW}" install krew
 )
-
-
+echo ""
+echo "Finished installation!"
 
