d2bcaf522adad00a770787847d210221b3f6d554
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2025-01-24 15:39:02 +0200

    fix configure.sh

--- configure.sh
+++ configure.sh
@@ -9,4 +9,3 @@ cp -r configs/espanso/ "$HOME/Library/Application Support/espanso"
 # zsh, omz, powerlevel10k
 cp configs/shell/omz/.p10k.zsh $HOME/
 cp configs/shell/zsh/.zshrc $HOME/
-p10k configure
