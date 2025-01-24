7bcc6b5b530a29b282c9ca3d9ad24c328f6ef3f8
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2025-01-24 15:05:25 +0200

    fix configure.sh

--- configure.sh
+++ configure.sh
@@ -2,8 +2,8 @@ set -e
 profile=$1
 
 # espanso
-cp -r common/configs/espanso/ "/Users/$HOME/Library/Application Support/"
-cp -r $profile/configs/espanso/ "/Users/$HOME/Library/Application Support/"
+cp -r common/configs/espanso/ "$HOME/Library/Application Support/"
+cp -r $profile/configs/espanso/ "$HOME/Library/Application Support/"
 
 # zsh, omz, powerlevel10k
 cp configs/shell/omz/.p10k.zsh $HOME/
