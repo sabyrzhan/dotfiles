b2be5bc734814d86dd93e34d68ab8e8c8af9e92e
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2025-01-24 15:12:29 +0200

    fix configure.sh

--- configure.sh
+++ configure.sh
@@ -3,8 +3,8 @@ profile=$1
 
 # espanso
 mkdir -p "$HOME/Library/Application Support/espanso"
-cp -r configs/espanso/ "$HOME/Library/Application Support/"
-cp -r configs/espanso/ "$HOME/Library/Application Support/"
+cp -r configs/espanso/ "$HOME/Library/Application Support/espanso"
+cp -r configs/espanso/ "$HOME/Library/Application Support/espanso"
 
 # zsh, omz, powerlevel10k
 cp configs/shell/omz/.p10k.zsh $HOME/
