decbc3272c57a3a45714bc495daf1d6e9ec4b854
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2025-01-29 23:29:19 +0200

    add xattr replaces to espanso and update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,17 +4,19 @@ profile=$1
 # espanso
 mkdir -p "$HOME/Library/Application Support/espanso"
 cp -R configs/espanso/ "$HOME/Library/Application Support/espanso/"
+echo "Finish espanso files copy!"
 
 # zsh, omz, powerlevel10k
 echo "Copying shell configs..."
-cp -i configs/shell/omz/.p10k.zsh $HOME/
-cp -i configs/shell/zsh/.zshrc $HOME/
-echo "Finished!"
+cp -i configs/shell/omz/.p10k.zsh $HOME/ || true
+cp -i configs/shell/zsh/.zshrc $HOME/ || true
+echo "Finished zsh files copy!"
 echo "Now manually reconfigure p10k: p10k configure"
 
 # karabiner
 mkdir -p $HOME/.config/karabiner
 cp -R configs/karabiner/ $HOME/.config/karabiner
+echo "Finished karabiner files copy!"
 
 # git configs
 cp -R -i configs/git/ $HOME/
